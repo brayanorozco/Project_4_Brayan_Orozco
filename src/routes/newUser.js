7f5f551d11c52c4e6f206f05db69f5b22b5bf7c0
brayanorozco <orozcotafur.brayan@gmail.com>
2021-07-07 09:26:55 +0200

    New user page last changes

--- src/routes/newUser.js
+++ src/routes/newUser.js
@@ -9,4 +9,34 @@ route.get("/", (req, res) => {
     
 });
 
+
+//Creating new account
+
+route.post('/', (req, res) => {
+
+    const name = req.body.firstName;
+    const lastName = req.body.lastName;
+    const newUserEmail = req.body.email;
+    const newUserPassword = req.body.password;
+
+    console.log(req);
+
+    connection.query(' INSERT INTO users (name, last_name, email_user, password) VALUES(?,?,?,?)',
+        [name, lastName, newUserEmail, newUserPassword],
+        (error, response) => {
+            if (error) throw error;
+
+            else {
+
+                req.session.user = {
+                    id: currentUser.id_user,
+                    email: currentUser.email_user,
+                    name: currentUser.name
+                }
+              
+              res.redirect("/home");
+            }
+        })
+})
+
 module.exports = route;
