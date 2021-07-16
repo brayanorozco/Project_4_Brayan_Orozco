ddac09c8c782cab157148c7004956d3387e12e28
brayanorozco <orozcotafur.brayan@gmail.com>
2021-07-16 09:26:32 +0200

    adding validations

--- src/routes/newUser.js
+++ src/routes/newUser.js
@@ -1,41 +1,57 @@
 //With this 'Router' method, we can call the routes written
 //in this module from another module.
 const route = require('express').Router();
+const {
+    body,
+    validationResult
+} = require('express-validator');
 const connection = require('../../config/dbConnetion');
 
 
 route.get("/", (req, res) => {
 
     res.render('newUser', {});
-    
+
 });
 
 
 //Creating new account
 
-route.post('/', (req, res) => {
+route.post('/',
+    body('firstName').notEmpty().isLength({
+        min: 2
+    }),
+    body('lastName').notEmpty().isLength({
+        min: 2
+    }),
+    body('email').isEmail(),
+    body('password').isLength({
+        min: 6
+    }),
 
-    const name = req.body.firstName;
-    const lastName = req.body.lastName;
-    const newUserEmail = req.body.email;
-    const newUserPassword = req.body.password;
+    (req, res) => {
 
-    connection.query(' INSERT INTO users (name, last_name, email_user, password) VALUES(?,?,?,?)',
-        [name, lastName, newUserEmail, newUserPassword],
-        (error, response) => {
-            if (error) throw error;
+        const name = req.body.firstName;
+        const lastName = req.body.lastName;
+        const newUserEmail = req.body.email;
+        const newUserPassword = req.body.password;
 
-            else {
+        connection.query(' INSERT INTO users (name, last_name, email_user, password) VALUES(?,?,?,?)',
+            [name, lastName, newUserEmail, newUserPassword],
+            (error, response) => {
+                if (error) throw error;
 
-                req.session.user = {
-                    id: response.id_user,
-                    email: response.email_user,
-                    name: response.name
+                else {
+
+                    req.session.user = {
+                        id: response.id_user,
+                        email: response.email_user,
+                        name: response.name
+                    }
+
+                    res.redirect("/home");
                 }
-              
-              res.redirect("/home");
-            }
-        })
-})
+            })
+    })
 
 module.exports = route;
